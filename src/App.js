f1161e972a54f5aafc2e32c98a23a6d977ff12d9
weld0 <peshkova-inna@mail.ru>
2023-10-15 23:17:28 +0300

    Update css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Services servisStr="Перечень оказываемых услуг"/>
+      <Services/>
+     
     </div>
   );
 }
---
+++ src/components/services/card/card.jsx
@@ -0,0 +1,14 @@
+import React  from "react";
+import styles from "./card.module.css";
+
+const Card = (props) =>{
+    return (
+        <div className={styles.card}>
+        <h3 className={styles.cardText}>{props.text}</h3>
+        <p className={styles.cardPost}>{props.post}</p>
+    </div>
+    )
+
+}
+
+export default Card;
