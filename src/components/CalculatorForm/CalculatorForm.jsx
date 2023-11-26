763be0cce300d1d2c3d1971c96e977ece5f4382e
Your Name <you@example.com>
2023-11-26 15:57:51 +0300

    UpDate CalculatorForm

--- src/components/CalculatorForm/CalculatorForm.jsx
+++ src/components/CalculatorForm/CalculatorForm.jsx
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 import Input from "../shared/Input/Input";
 import InputAdornment from "@mui/material/InputAdornment";
-import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/MenuItem";
 import SelectField from "../shared/Select/Select";
+import { useForm } from "react-hook-form";
 
 const orderForms = [
-  { label: "VIP тариф (более 100м)", value: "s1" },
-  { label: "Стандарт", value: "s2" },
-  { label: "План", value: "s3" },
+  { label: "VIP тариф (более 100м)", value: 1400 },
+  { label: "Стандарт", value: 1600 },
+  { label: "План", value: 500 },
 ];
 
 const CalculatorForm = (props) => {
-  const [squareMetr, setSquareMetr] = useState(null);
   const [tarif, setTarif] = useState(null);
+  const [result, setResult] = useState(null);
+  const {
+    register,
+    formState: { //объект у которого есть различные свойства
+      errors, //свойство проверки на ошибки
+      isValid 
+  },
+  watch,
+    trigger,
+    getValues,
+  } = useForm({
+    mode:"onChange"
+  })
 
-  function onSquareMetrChange(event) {
-    setSquareMetr(event.target.value);
-  }
+  const squareMetr = watch("squareMetr")
   function onTarifChange(event) {
+    trigger("squareMetr")
     setTarif(event.target.value);
+  }
+
+  function onCalculate() {
+   setResult( squareMetr * tarif)
+   console.log (squareMetr, tarif, (getValues()) )
+
   }
 
   return (
@@ -28,17 +46,23 @@ const CalculatorForm = (props) => {
       }}
       style={{ display: "flex", flexDirection: "column", gap: "15px" }}
     >
-      <Input
+      <Input 
         label="Метры квадратные"
         sx={{ m: 1, width: "25ch" }}
         type="number"
         placeholder="Введите количество метров"
-        InputProps={{
+        InputProps={{ ...register("squareMetr",{
+          required: "Поле обязательно к заполнению", //проверка на заполнение формы
+          min: { // проверка на заполнение ко-во введенных символов
+              value: tarif === 1400? 100:0,
+              message: 'должно быть не менее 100 м'
+          }
+      }),
           startAdornment: <InputAdornment position="start">m2</InputAdornment>,
         }}
-        onChange={onSquareMetrChange}
-        value={squareMetr}
+       
       />
+      <div>{errors?.squareMetr && <p>{errors?.squareMetr?.message || "Error!"}</p>}</div>
       <div>
         <SelectField
           options={orderForms}
@@ -46,7 +70,15 @@ const CalculatorForm = (props) => {
           placeholder="Выберите тариф"
           value={tarif}
           onChange={onTarifChange}
+          InputProps={{ ...register("tarif",{
+            required: "Поле обязательно к заполнению", //проверка на заполнение формы
+           
+        })}}
         />
+          <div>{errors?.tarif && <p>{errors?.tarif?.message || "Error!"}</p>}</div>
+      <Button variant="outlined" onClick={onCalculate} >Расчитать</Button>
+      <span>{result}</span>
+
       </div>
     </form>
   );
