cd79e2b863bf525d9807e81ffc70ee7d018f7952
Your Name <you@example.com>
2023-12-10 16:39:14 +0300

    The calculator is working

--- src/components/CalculatorForm/CalculatorForm.jsx
+++ src/components/CalculatorForm/CalculatorForm.jsx
@@ -1,89 +1,112 @@
 import React, { useState } from "react";
 import Input from "../shared/Input/Input";
 import InputAdornment from "@mui/material/InputAdornment";
-import Button from "@mui/material/MenuItem";
-import SelectField from "../shared/Select/Select";
 import { useForm } from "react-hook-form";
+import styles from "./CalculatorForm.module.css";
+import { Controller } from "react-hook-form";
+import ReactSelect from "react-select";
+import MyButton from "../shared/Button/MyButton";
 
-const orderForms = [
-  { label: "VIP тариф (более 100м)", value: 1400 },
-  { label: "Стандарт", value: 1600 },
-  { label: "План", value: 500 },
+const options = [
+  { value: 1400, label: "VIP тариф (более 100м)" },
+  { value: 1600, label: "Стандарт" },
+  { value: 500, label: "План" },
 ];
 
+const getValue = (value) => {
+  return value ? options.find((option) => option.value === value) : "";
+};
+
 const CalculatorForm = (props) => {
-  const [tarif, setTarif] = useState(null);
-  const [result, setResult] = useState(null);
+   const [result, setResult] = useState(0);
   const {
     register,
-    formState: { //объект у которого есть различные свойства
-      errors, //свойство проверки на ошибки
-      isValid 
-  },
-  watch,
-    trigger,
-    getValues,
+    control,
+    formState: {
+           errors, //свойство проверки на ошибки
+      isValid,
+    },
+    handleSubmit,
+    watch,
+    reset,
+    
   } = useForm({
-    mode:"onBlur"
-  })
+    mode: "onChange",
+    defaultValues:{
+      tarif: 1600,
+      squareMetr: null
+    }
+  });
+  const onSubmit = (data) => {
+    reset();
+  };
+  const squareMetr = watch("squareMetr");
+  const tarif = watch("tarif");
+  
+ 
+    function onCalculate() {
+      if (tarif === 1400 && squareMetr < 100) {
+        return ; // расчет не производится
+      }
+      setResult(squareMetr * tarif);
+        reset();
+    }
+    
 
-  const squareMetr = watch("squareMetr")
-  function onTarifChange(event) {
-    trigger("squareMetr")
-    setTarif(event.target.value);
-    alert(tarif);
-  }
-
-  function onCalculate() {
-   setResult( squareMetr * tarif)
-   console.log (squareMetr, tarif, (getValues()) )
-
-  }
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-      style={{ display: "flex", flexDirection: "column", gap: "15px" }}
-    >
-      <Input 
+    <form className={styles.calculationForm} onSubmit={handleSubmit(onSubmit)}>
+       <Controller
+          control={control}
+          name="tarif"              
+          
+          render={({ field: { onChange, value }, fieldState: { error } }) => (
+            <div>
+              <ReactSelect
+                placeholder="Taриф"
+                options={options}
+                value={getValue(value)}
+                onChange={(newValue) => onChange(newValue.value)}
+                              />
+              {error && <div style={{ color: "red" }}>{error?.message}</div>}
+            </div>
+          )}
+          />
+          
+      <Input
+        className={styles.calculationInput}
         label="Метры квадратные"
         sx={{ m: 1, width: "25ch" }}
         type="number"
         placeholder="Введите количество метров"
-        InputProps={{ ...register("squareMetr",{
-          required: "Поле обязательно к заполнению", //проверка на заполнение формы
-          min: { // проверка на введенное кол-во метров
-              value: tarif === 1400? 100:0,
-              message: 'должно быть не менее 100 м'
-          }
-      }),
+        InputProps={{
+          ...register("squareMetr", {
+            required: "Поле обязательно к заполнению", //проверка на заполнение формы
+            min: {
+              // проверка на введенное кол-во метров
+              value: tarif === 1400 ? 100 : 0,
+              message: "должно быть не менее 100 м",
+            },
+          }),
           startAdornment: <InputAdornment position="start">m2</InputAdornment>,
         }}
-       
       />
-      <div>{errors?.squareMetr && <p>{errors?.squareMetr?.message || "Error!"}</p>}</div>
       <div>
-        <SelectField
-          options={orderForms}
-          label="Тариф"
-          placeholder="Выберите тариф"
-          value={tarif}
-          onChange={onTarifChange}
-          InputProps={{ ...register("tarif",{
-            required:{
-              value: tarif != null? "Выберите тариф": "",
-            }  //проверка на заполнение формы
-            
-           
-        }),}}
-        />
-          <div>{errors?.tarif && <p>{errors?.tarif?.message || "Error!"}</p>}</div>
-      <Button variant="outlined" onClick={onCalculate}  >Расчитать</Button>
-      <span>{result}</span>
-
+        {errors?.squareMetr && <p>{errors?.squareMetr?.message || "Error!"}</p>}
       </div>
+      <div>       
+        
+        <MyButton
+          
+          variant="outlined"
+          onClick={onCalculate}
+         >
+          Расчитать
+        </MyButton>
+        
+        <div> <span className={styles.calculationResult}>{`${result} `} </span> </div>
+       </div>
+      
     </form>
   );
 };
