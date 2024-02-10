9795df96bb775cc5f64f01e6b34c395bd2ca849f
Your Name <you@example.com>
2024-02-10 12:57:52 +0300

    rename

--- src/components/portfolio/workCards/workCards.jsx
+++ src/components/portfolio/workCards/workCards.jsx
@@ -2,7 +2,7 @@ import style from "./WorkCards.module.css";
 import PortfolioDetalis from "../../Slider/PortfolioDetalis";
 import React, { useState } from "react";
 import Dialog from "../../shared/Dialog/Dialog";
-
+//Галерея работ на главной странице 
 const WorkCards = (props) => {
   const [state, setState] = useState(null);
   const [openDialog, setOpenDialog] = useState(false)
@@ -20,7 +20,7 @@ function onDialogCancel(){
     { image: "/work/order3.jpg", id: "order3" },
     { image: "/work/order4.jpg", id: "order4" },
     { image: "/work/order5.jpg", id: "order5" },
-    { image: "/work/order6.jpg",   },
+    { image: "/work/order6.jpg",  id: "order6" },
   ];
   const workCardsComponents = workPiсtures.map((picture) => {
     return (
